8155d213cc0c32b25a441c7233b838f5ba740b99
3Rhizoaui <h3hizaoui@gmail.com>
2023-08-26 20:49:28 +0200

    test Commit

---
+++ Object Repository/New Test Object.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>32b600b9-f646-4e35-9566-8e369245476e</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
